410a712cdea1ed15c01df21996c6dd61d4d96b4c
eerickwrld <160683152+eerickwrld@users.noreply.github.com>
2026-02-10 08:32:24 -0300

    Update CORS configuration to allow all origins

--- plastikatwy-site-backend-7606bba907d3/plastikatwy-site-backend-7606bba907d3/config/cors.php
+++ plastikatwy-site-backend-7606bba907d3/plastikatwy-site-backend-7606bba907d3/config/cors.php
@@ -1,17 +1,13 @@
 <?php
 
 return [
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', '*'],
     
     'allowed_methods' => ['*'],
     
-    'allowed_origins' => [
-        env('FRONTEND_URL', 'https://alcast-site-yjjm.vercel.app'),
-    ],
+    'allowed_origins' => ['*'],
     
-    'allowed_origins_patterns' => [
-        '#^https://.*\.vercel\.app$#',
-    ],
+    'allowed_origins_patterns' => [],
     
     'allowed_headers' => ['*'],
     
