e50a99693920274f461181adca04d579859aa0ca
eerickwrld <160683152+eerickwrld@users.noreply.github.com>
2026-02-12 11:36:50 -0300

    Update upload-images route to return HTML form

--- plastikatwy-site-backend-7606bba907d3/plastikatwy-site-backend-7606bba907d3/routes/web.php
+++ plastikatwy-site-backend-7606bba907d3/plastikatwy-site-backend-7606bba907d3/routes/web.php
@@ -1,17 +1,20 @@
-<?php
-
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::get('/upload-images', function() {
-    return view('upload-images');
+    return '<!DOCTYPE html>
+<html>
+<head><title>Upload</title></head>
+<body>
+<h1>Upload de Imagens</h1>
+<form action="/upload-images" method="POST" enctype="multipart/form-data">
+<input type="hidden" name="_token" value="' . csrf_token() . '">
+<input type="file" name="images[]" multiple accept="image/*">
+<br><br>
+<button type="submit">Enviar Imagens</button>
+</form>
+</body>
+</html>';
 });
 
-Route::post('/upload-images', function(Request $request) {
+Route::post('/upload-images', function(\Illuminate\Http\Request $request) {
     if ($request->hasFile('images')) {
         $uploadedCount = 0;
         foreach ($request->file('images') as $image) {
